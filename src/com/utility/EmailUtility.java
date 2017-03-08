2c0e23fa6c35b0744a53cbfb65eadc4495ace0be
Deepak Mahana <mahana.deepak20@gmail.com>
2017-03-08 11:56:13 +0530

    Update EmailUtility.java

--- src/com/utility/EmailUtility.java
+++ src/com/utility/EmailUtility.java
@@ -18,7 +18,7 @@ public class EmailUtility {
 		  
 		  //Get the session object  
 		  Properties props = new Properties();  
-		  props.put("mail.smtp.host", "mail.kitecertification.com");  
+		  props.put("mail.smtp.host", "www.gmail.com");  
 		  props.put("mail.smtp.socketFactory.port", "25");  
 		  //props.put("mail.smtp.socketFactory.class",  
 		           // "javax.net.ssl.SSLSocketFactory");  
@@ -28,16 +28,16 @@ public class EmailUtility {
 		  Session session = Session.getInstance(props,  
 		   new javax.mail.Authenticator() {  
 		   protected PasswordAuthentication getPasswordAuthentication() {  
-		   return new PasswordAuthentication("info@kitecertification.com","kitecert123info");//change accordingly  
+		   return new PasswordAuthentication("mahana.deepak20@gmail.com","*");//change accordingly  
 		   }  
 		  });  
 		   
 		  //compose message  
 		  try {  
 		   MimeMessage message = new MimeMessage(session);  
-		   message.setFrom(new InternetAddress("info@kitecertification.com"));//change accordingly  
+		   message.setFrom(new InternetAddress("www.gmail.com"));//change accordingly  
 		   message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-		   message.setSubject("Verify Your Email - Khateeb Institute");  
+		   message.setSubject("Verify Your Email");  
 		   message.setText("Your Email Verification Number  \n Enter with minus sign if present before the number\n\n" + num);  
 		     
 		   //send message  
